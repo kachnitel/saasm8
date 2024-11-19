6f8586675d81f819f844f4a4039f79c0bed5e993
Ondrej Vana <vana.ondrej@gmail.com>
2024-11-18 16:22:22 -0800

    chore: Update .gitignore to exclude temporary files

--- src/Entity/TimeEntry.php
+++ src/Entity/TimeEntry.php
@@ -6,17 +6,6 @@ use App\Repository\TimeEntryRepository;
 use Doctrine\DBAL\Types\Types;
 use Doctrine\ORM\Mapping as ORM;
 
-/**
- * @method int|null                getId()
- * @method \DateTimeInterface|null getStartTime()
- * @method \DateTimeInterface|null getEndTime()
- * @method string|null             getNote()
- * @method BillingCategory|null    getBillingCategory()
- * @method self                    setStartTime(\DateTimeInterface|null $startTime)
- * @method self                    setEndTime(\DateTimeInterface|null $endTime)
- * @method self                    setNote(string|null $note)
- * @method self                    setBillingCategory(BillingCategory|null $billingCategory)
- */
 #[ORM\Entity(repositoryClass: TimeEntryRepository::class)]
 class TimeEntry
 {
